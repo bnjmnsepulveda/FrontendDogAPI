8f73d07d9a4e88481386691bfe865f3920cf9f4d
Benjamin Sepulveda <benjamin@MacBook-Pro-de-Benjamin.local>
2019-06-23 16:24:14 -0400

    se agrega rutas para detalles de perritos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListaRazasComponent } from './components/lista-razas/lista-razas.component';
+import { DetalleRazaComponent } from './components/detalle-raza/detalle-raza.component';
 
 const routes: Routes = [{
   path: '',
   component: ListaRazasComponent
+},
+{
+  path: 'raza/:nombre',
+  component: DetalleRazaComponent
 }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaRazasComponent } from './components/lista-razas/lista-razas.component';
+import { DetalleRazaComponent } from './components/detalle-raza/detalle-raza.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaRazasComponent
+    ListaRazasComponent,
+    DetalleRazaComponent
   ],
   imports: [
     BrowserModule,
