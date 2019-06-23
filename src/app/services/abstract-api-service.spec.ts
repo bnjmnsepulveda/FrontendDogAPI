e363a3e4a9e4a7f0e8c6735c1db3ade0b4a82449
Benjamin Sepulveda <benjamin@MacBook-Pro-de-Benjamin.local>
2019-06-23 12:13:30 -0400

    se agrega abstract api class template para peticiones http

--- src/app/services/abstract-api-service.spec.ts
+++ src/app/services/abstract-api-service.spec.ts
@@ -2,6 +2,6 @@ import { AbstractApiService } from './abstract-api-service';
 
 describe('AbstractApiService', () => {
   it('should create an instance', () => {
-    expect(new AbstractApiService()).toBeTruthy();
+    expect(new AbstractApiService(null)).toBeTruthy();
   });
 });
--- src/app/services/abstract-api-service.ts
+++ src/app/services/abstract-api-service.ts
@@ -1,2 +1,42 @@
-export class AbstractApiService {
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
+const HTTP_OPTIONS = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
+export class AbstractApiService<T> {
+
+  constructor(private _http: HttpClient) { }
+
+  protected get(url: string, params?: HttpParams | { [ param: string ]: string | string[]}): Observable<T> {
+    const httpOptions = {
+      'params': params
+    };
+    return this._http.get<T>(url, httpOptions)
+    .pipe(
+      catchError(this.handleException)
+    );
+  }
+
+  protected getAll(url: string): Observable<T[]> {
+    return this._http.get<T[]>(url, HTTP_OPTIONS)
+    .pipe(
+      catchError(this.handleException)
+    );
+  }
+
+   /**
+  * manejo de excepciones 404.
+  * @param error
+  */
+ protected handleException(error: HttpErrorResponse) {
+  if (error.status === 404 || error.status === 500) {
+    return of(null);
+  }
+  throw error;
+}
 }
