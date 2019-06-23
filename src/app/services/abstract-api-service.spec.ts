0a0a1e7cc1daa2828dce63f889b67d4979e7ab45
Benjamin Sepulveda <benjamin@MacBook-Pro-de-Benjamin.local>
2019-06-23 12:09:35 -0400

    inicio repositorio

---
+++ src/app/services/abstract-api-service.spec.ts
@@ -0,0 +1,7 @@
+import { AbstractApiService } from './abstract-api-service';
+
+describe('AbstractApiService', () => {
+  it('should create an instance', () => {
+    expect(new AbstractApiService()).toBeTruthy();
+  });
+});
